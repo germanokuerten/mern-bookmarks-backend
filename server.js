9cbb966cdca5d3a5f3aac291f0bb02d0a567cfc7
chess2022 <lakeshore.hnh@gmail.com>
2022-06-01 20:44:42 -0700

    imports dependencies

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-hello
-testing 
+require("dotenv").config()
+const { PORT = 3031, DATATBASE_URL }=process.env
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const cors = require("cors")
+const app = express()
+
+
+
+
+
+app.listen(PORT, () => console.log(`We are listening on PORT ${PORT}`))
